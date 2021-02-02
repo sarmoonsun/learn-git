a033c808ba2f3653e9d0e051abff286e811f2578
samane <71874473+srmoon400@users.noreply.github.com>
2021-02-02 19:27:03 +0330

    Iman

--- src/Program.cs
+++ src/Program.cs
@@ -6,7 +6,21 @@ namespace src
     {
         static void Main(string[] args)
         {
+            String name; 
             Console.WriteLine("Hello World!");
+            Console.WriteLine("please enter your name");
+            name = Console.ReadLine();
+            if (name == "Iman" )
+            {
+                Console.WriteLine("your name is correct");
+               
+            }
+            else
+               {
+                   Console.WriteLine($"u dont know your name hahahah");
+                   
+               } 
+            Console.WriteLine($" your name is Iman");
         }
     }
 }
